6e3263f33ad7dde94a2b2159e6a1f8e0740bc252
yamato0211 <kumayama0211@gmail.com>
2022-10-31 00:21:55 +0900

    [add] server with gin-gonic

--- server.go
+++ server.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"first-hackathon/db"
+	"first-hackathon/directives"
 	"first-hackathon/graph"
 	"first-hackathon/graph/generated"
+	"first-hackathon/middlewares"
+	"first-hackathon/utils"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func graphqlHandler() gin.HandlerFunc {
 	// NewExecutableSchema and Config are in the generated.go file
 	// Resolver is in the resolver.go file
-	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	c := generated.Config{Resolvers: &graph.Resolver{}}
+	c.Directives.Auth = directives.Auth
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(c))
 
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
@@ -30,6 +37,29 @@ func playgroundHandler() gin.HandlerFunc {
 
 func main() {
 	r := gin.Default()
+	utils.LoadEnv()
+	db.InitDB()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"*",
+		},
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"PATCH",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+	}))
+	r.Use(middlewares.AuthMiddleware())
 	r.POST("/query", graphqlHandler())
 	r.GET("/", playgroundHandler())
 	r.Run()
