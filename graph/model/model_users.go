2c90b8a45ce0cc3438c6d4455184f0f85a02f03a
yamato0211 <kumayama0211@gmail.com>
2022-11-02 12:50:33 +0900

    [change] unique column

--- graph/model/model_users.go
+++ graph/model/model_users.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	ID       string `json:"id" gorm:"type:varchar(255);primaryKey"`
-	Name     string `json:"name" gorm:"type:varchar(100);not null;unique"`
-	Email    string `json:"email" gorm:"type:varchar(100);not null"`
+	Name     string `json:"name" gorm:"type:varchar(100);not null"`
+	Email    string `json:"email" gorm:"type:varchar(100);not null;unique"`
 	Password string `json:"password" gorm:"type:varchar(100);not null"`
 }
